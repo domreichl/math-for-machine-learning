21c3a2cb371bd9c44446d06fb7a16d6adfae9ef8
Dominic Reichl <42763908+domreichl@users.noreply.github.com>
2018-09-21 17:02:46 +0200

    Update math_for_machine_learning.py

--- math_for_machine_learning.py
+++ math_for_machine_learning.py
@@ -3,47 +3,33 @@ math_for_machine_learning.py by Dominic Reichl, @domreichl
 collects mathematical concepts that are essential for machine learning
 '''
 
-from elementary_algebra import linear_function # for plotting degree 1 polynomial
-from elementary_algebra import quadratic_function # for plotting degree 2 polynomial
+import elementary_algebra as ea
+import linear_algebra as la
+import differential_calculus as dc
+import integral_calculus as ic
 
-linear_function(3, 9, (-5, 6), 1, annoInter=False)
+ea.linear_function(3, 9, (-5, 6), 1, annoInter=False) # degree 1 polynomial
 ''' plot y = 3x + 9
     data range for x: -5 to 5
     slope length: 1x
     don't plot intercept annotation ''' 
-quadratic_function(2, 8, -16, (-20, 21), xInter=True, yInter=False, vertex=True, symLine=False)
+ea.quadratic_function(2, 8, -16, (-20, 21), xInter=True, yInter=False, vertex=True, symLine=False) # degree 2 polynomial
 ''' plot y = 2x**2 + 8x - 16
     data range for x: -20 to 20
     plot roots (xInter) and vertex, but not y intercept nor line of symmetry '''
 
-from linear_algebra import vector_properties # for plotting 2d vectors and returning their magnitude and direction
-from linear_algebra import vector_addition # for plotting two 2d vectors and their sum before returning the latter
-from linear_algebra import vector_multiplication # for multiplying with vectors and returning scalar, dot, or cross products
-from linear_algebra import matrix_operations # for adding, subtracting, negating, transposing, multiplying, and dividing matrices
-from linear_algebra import solve_system_of_equations # for using matrices to solve systems of equations
-from linear_algebra import eigenvalues_eigenvectors # for calculating eigenvalues and eigenvectors of matrices
+print(la.vector_properties(9, 9)) # plot and print magnitude and amplitude of a 2d vector
+print(la.vector_addition([9,7], [-1,-2])) # plot and print the sum of two 2d vectors
+print(la.vector_multiplication([2,3,1], [1,2,-2])) # multiply two 3d vectors
+print(la.matrix_operations([[1,2,3],[4,5,6]], [[6,5,4],[3,2,1]], 'div')) # inversely multiply two 3x3 matrices
+la.solve_system_of_equations([[4,2],[5,2]], [[14],[16]]) # 2x+4y=18; 6x+2y=34; find x and y
+la.eigenvalues_eigenvectors([[2,0],[0,3]]) # print eigenvalues and eigenvectors of a 2x2 matrix
 
-print(vector_properties(9, 9)) # get magnitude and amplitude of a 2x1 vector
-print(vector_addition([9,7], [-1,-2])) # add two 2x1 vectors
-print(vector_multiplication([2,3,1], [1,2,-2])) # multiply two 3x1 vectors
-print(matrix_operations([[1,2,3],[4,5,6]], [[6,5,4],[3,2,1]], 'div')) # inversely multiply two 3x3 matrices
-solve_system_of_equations([[4,2],[5,2]], [[14],[16]]) # 2x+4y=18; 6x+2y=34; find x and y
-eigenvalues_eigenvectors([[2,0],[0,3]]) # get eigenvalues and eigenvectors of a 2x2 matrix
+dc.average_rate_of_change() # introduces the concepts 'secant line' and 'slope'
+dc.limits() # introduces the concept of limits
+dc.discontinuity() # introduces the concept of a function being non-continuous at a given point
+dc.differentiation() # introduces the concept of derivatives
+dc.critical_points() # introduces the concept of critical points
+dc.partial_derivatives() # introduces the concept of partial derivatives for multivariate functions
 
-from differential_calculus import average_rate_of_change
-from differential_calculus import limits
-from differential_calculus import discontinuity
-from differential_calculus import differentiation
-from differential_calculus import critical_points
-from differential_calculus import partial_derivatives
-
-average_rate_of_change() # introduces the concepts 'secant line' and 'slope'
-limits() # introduces the concept of limits
-discontinuity() # introduces the concept of a function being non-continuous at a given point
-differentiation() # introduces the concept of derivatives
-critical_points() # introduces the concept of critical points (min, max, flex)
-partial_derivatives() # introduces the concept of partial derivatives for multivariate functions
-
-from integral_calculus import integral # for calculating an integral and plotting its area
-
-integral() # no arguments because f(x) = 3x²+2x+1 and integral limits (0,3) are hardcoded in module
+ic.integral() # no arguments because f(x) = 3x²+2x+1 and integral limits (0,3) are hardcoded in module
