031db66af293fb57177dbb510e391531f018f4b8
Dominic Reichl <42763908+domreichl@users.noreply.github.com>
2018-09-21 02:10:56 +0200

    Update math_for_machine_learning.py

--- math_for_machine_learning.py
+++ math_for_machine_learning.py
@@ -5,6 +5,7 @@ collects mathematical concepts that are essential for machine learning
 
 from elementary_algebra import linear_function # for plotting degree 1 polynomial
 from elementary_algebra import quadratic_function # for plotting degree 2 polynomial
+
 linear_function(3, 9, (-5, 6), 1, annoInter=False)
 ''' plot y = 3x + 9
     data range for x: -5 to 5
@@ -21,6 +22,7 @@ from linear_algebra import vector_multiplication # for multiplying with vectors 
 from linear_algebra import matrix_operations # for adding, subtracting, negating, transposing, multiplying, and dividing matrices
 from linear_algebra import solve_system_of_equations # for using matrices to solve systems of equations
 from linear_algebra import eigenvalues_eigenvectors # for calculating eigenvalues and eigenvectors of matrices
+
 print(vector_properties(9, 9)) # get magnitude and amplitude of a 2x1 vector
 print(vector_addition([9,7], [-1,-2])) # add two 2x1 vectors
 print(vector_multiplication([2,3,1], [1,2,-2])) # multiply two 3x1 vectors
@@ -30,4 +32,5 @@ eigenvalues_eigenvectors([[2,0],[0,3]]) # get eigenvalues and eigenvectors of a 
 
 
 from integral_calculus import integral
+
 integral() # no arguments because f(x) = 3x²+2x+1 and integral limits (0,3) are hardcoded in module
