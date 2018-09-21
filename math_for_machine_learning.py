afbf88e432679df5fa69f6b90ecba21ada4cdf9f
Dominic Reichl <42763908+domreichl@users.noreply.github.com>
2018-09-21 02:10:20 +0200

    Update math_for_machine_learning.py

--- math_for_machine_learning.py
+++ math_for_machine_learning.py
@@ -5,14 +5,11 @@ collects mathematical concepts that are essential for machine learning
 
 from elementary_algebra import linear_function # for plotting degree 1 polynomial
 from elementary_algebra import quadratic_function # for plotting degree 2 polynomial
-
 linear_function(3, 9, (-5, 6), 1, annoInter=False)
 ''' plot y = 3x + 9
     data range for x: -5 to 5
     slope length: 1x
-    don't plot intercept annotation '''
-
-    
+    don't plot intercept annotation ''' 
 quadratic_function(2, 8, -16, (-20, 21), xInter=True, yInter=False, vertex=True, symLine=False)
 ''' plot y = 2x**2 + 8x - 16
     data range for x: -20 to 20
@@ -24,7 +21,6 @@ from linear_algebra import vector_multiplication # for multiplying with vectors 
 from linear_algebra import matrix_operations # for adding, subtracting, negating, transposing, multiplying, and dividing matrices
 from linear_algebra import solve_system_of_equations # for using matrices to solve systems of equations
 from linear_algebra import eigenvalues_eigenvectors # for calculating eigenvalues and eigenvectors of matrices
-
 print(vector_properties(9, 9)) # get magnitude and amplitude of a 2x1 vector
 print(vector_addition([9,7], [-1,-2])) # add two 2x1 vectors
 print(vector_multiplication([2,3,1], [1,2,-2])) # multiply two 3x1 vectors
